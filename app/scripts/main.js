eebfa33145bb7a666046e09b5e73d16ca0b27ef4
adhilnv <adhilazeeznv@gmail.com>
2013-09-01 23:48:36 +0530

    First Commit

---
+++ app/scripts/main.js
@@ -0,0 +1,14 @@
+require.config({
+    paths: {
+        jquery: '../bower_components/jquery/jquery',
+    },
+    shim: {
+    }
+});
+
+require(['app', 'jquery'], function (app, $) {
+    'use strict';
+    // use app here
+    console.log(app);
+    console.log('Running jQuery %s', $().jquery);
+});
